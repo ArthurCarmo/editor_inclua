5ad5fc23e9c97337c3a8f2b2da307482eba1b565
Arthur <arthur.goncalves.carmo@gmail.com>
2019-07-01 08:48:33 -0300

    modified:   editor_qt.py

--- editor_qt.py
+++ editor_qt.py
@@ -12,9 +12,8 @@ class Main(QtWidgets.QMainWindow):
         self.xephyr 		= QProcess(self)
         self.xeyes 		= QProcess(self)
         self.window_manager	= QProcess(self)
-        self.unique_meaning	= []
-        self.multiple_meaning	= []
-        self.automaton		= ahocorasick.Automaton()
+        self.unique_meaning	= ahocorasick.Automaton()
+        self.multiple_meaning	= ahocorasick.Automaton()
         
         self.initUI()
         
@@ -34,20 +33,23 @@ class Main(QtWidgets.QMainWindow):
     def initUI(self):
         self.splitter	= QtWidgets.QSplitter(self)
         self.text	= QtWidgets.QTextEdit()
-        self.cursor	= self.text.textCursor()
         
         self.btn_box	= QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.LeftToRight, self.text)
-        self.btn_open	= QtWidgets.QPushButton()
-        self.btn_text 	= QtWidgets.QPushButton()
+        btn_open	= QtWidgets.QPushButton()
+        btn_text 	= QtWidgets.QPushButton()
+        show_cursor	= QtWidgets.QPushButton()
         
-        self.btn_open.setText("Abrir Visualizador");
-        self.btn_text.setText("Enviar Texto");
+        btn_open.setText("Abrir Visualizador")
+        btn_text.setText("Enviar Texto")
+        show_cursor.setText("Posições do cursor")
         
-        self.btn_open.clicked.connect(self.runProcess)
-        self.btn_text.clicked.connect(self.getText)
+        btn_open.clicked.connect(self.runProcess)
+        btn_text.clicked.connect(self.getText)
+        show_cursor.clicked.connect(self.print_cursor)
         
-        self.btn_box.addWidget(self.btn_open)
-        self.btn_box.addWidget(self.btn_text)
+        self.btn_box.addWidget(btn_open)
+        self.btn_box.addWidget(btn_text)
+        self.btn_box.addWidget(show_cursor)
         
         self.text.setLayout(self.btn_box)
         
@@ -62,13 +64,38 @@ class Main(QtWidgets.QMainWindow):
         self.setGeometry(0, 0, 800, 600)
         self.setWindowTitle("Inclua")
 
+    def print_cursor(self):
+    	cursor = self.text.textCursor()
+    	print("position:%2d\nachor:%5d\n" % (cursor.position(), cursor.anchor()))
+
     def getText(self):
-        self.cursor = self.text.textCursor()
-        if self.cursor.hasSelection():
-    	        text = self.cursor.selection().toPlainText()
-        else:
+        cursor = self.text.textCursor()
+        if cursor.hasSelection():
+    	        text = cursor.selection().toPlainText()
+        else: #Deixa o texto alternando vermelho e preto
                 text = self.text.toPlainText()
-                self.text.clear()
+                init_pos = cursor.position()
+                cursor.movePosition(cursor.Start)
+                k = 1
+                while cursor.movePosition(cursor.NextWord, cursor.KeepAnchor) :
+                        cursor.movePosition(cursor.StartOfWord)
+                        if k % 2 == 1:
+                            cursor.select(cursor.WordUnderCursor)
+                            self.text.setTextCursor(cursor)
+                            red = QtGui.QColor(0xFF0000)
+                            self.text.setTextColor(red)
+                       	else :
+                      	    cursor.select(cursor.WordUnderCursor)
+                      	    self.text.setTextCursor(cursor)
+                      	    black = QtGui.QColor(0x0)
+                      	    self.text.setTextColor(black)
+                        print("word:", cursor.selection().toPlainText(), end="|")
+                        print(cursor.anchor(), cursor.position())
+                        cursor.clearSelection()
+                        k+=1
+                cursor.setPosition(init_pos)
+                self.text.setFocus()
+                self.text.setTextCursor(cursor)
         print(text)
     
     def runProcess(self):
