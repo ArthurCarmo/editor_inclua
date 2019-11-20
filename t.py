fd27af41fca62754a9da71128e65ffae215a25a1
Arthur Carmo <arthur.goncalves.carmo@gmail.com>
2019-11-20 14:24:20 -0300

    Exporta corretamente

--- t.py
+++
@@ -1,5 +0,0 @@
-import sys
-from PyQt5 import QtWidgets
-
-QtWidgets.QApplication(sys.argv)
-QtWidgets.QFileDialog().getOpenFileName(caption="Abrir documento", filter="Documentos (*.pdf *.odt *.doc *.docx *.ppt *.pptx *.rtf *.pps *.ppsx *.odp);; Todos os arquivos (*.*)")
